d74d690360b8d7c6043f9ac23830c542549fd72d
Omotayo <113688693+thomasomotayo@users.noreply.github.com>
2023-07-27 21:58:37 +0100

    Add files via upload

---
+++ My_Web_App/conn.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "ethomdigital";
+$db   = "cee_db";
+$conn = null;
+
+try {
+  $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
+} catch (Exception $e) {
+  
+}
+
+
+ ?>
